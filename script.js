1ce74d910d1523dfa1f83c63e9d1e1635e183dd4
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2021-11-23 17:11:33 -0800

    created initial HTML content, added CSS rules

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "They are huskies!";
+h2.textContent = "They are huskies! And they love to...";
+document.querySelector("body").appendChild(h2);
